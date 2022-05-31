31a2dc4a6e366f1a50c3df218ceb2f181709d003
Franco Silvetti <franco.silvetti@gmail.com>
2022-05-31 18:53:09 -0300

    Desafio eventos

--- main.js
+++ main.js
@@ -1,81 +1,33 @@
-alert("Bienvenido a nuestro sistema de solicitud de créditos para empresas constructoras. A continuación lo invitamos a completar los datos que nos solicita el simulador.")
-let montoAprobado = 0;
-let incrementoporObras = 0;
-let incrementoporTrayectoria=0;
-let incremento = 0;
-let monto=0;
-let calificacion = prompt("Bienvenido, por favor para iniciar esta solicitud, necesitamos que ingrese su calificación crediticia: A, B o C ");;
-let provincia = prompt("Ingrese la Provincia en la cual se encuentra");
-let obras=parseFloat(prompt("Ingrese la Cantidad de obras que su organización ha realizado "));
-let trayectoria=parseFloat(prompt("Ingrese la Cantidad de años de trayectoria en la construcción"));
+/* Definición de Variables */
+let turno=parseInt(prompt("Por favor ingrese su permanencia en hs: 4, 8 , 12 o 24"));
+let valor=100;
+let monto=turno*valor;
 
-class Beneficiario {
-    constructor (nombre, empresa, mail){
-        this.nombre = prompt("Ingrese el nombre y apellido del solicitante").toUpperCase();
-        this.mail = prompt("Ingrese el correo eletrónico del solicitante").toUpperCase()
-        this.empresa = prompt("Ingrese el nombre de la Empresa");
-            
-    }
-}
-const Beneficiario1 = new Beneficiario();
-console.log(Beneficiario1);
+/* Muestra del cálculo del monto de la estadía */
 
-function calculoPorCalificacion(){
-    if (calificacion == "A"){
-        montoAprobado=2000000;
-        console.log(montoAprobado);
-        
-    } else if (calificacion == "B"){
-        montoAprobado = 1500000;
-        console.log(montoAprobado);
-    } else if (calificacion =="C"){
-        montoAprobado = 1000000;
-        console.log(montoAprobado);
-    }else{
-        alert("Usted no posee la calificación crediticia mínima para la gestión del crédito");
-    }
-}
-calculoPorCalificacion();
-
-function calculoPorObras(){
-    if(obras >=10){
-        incrementoporObras = 250000;
-        console.log(incrementoporObras);
-    }else{
-        console.log(incrementoporObras);
-    }
-}
-calculoPorObras();
-
-function calculoPorTrayectoria(){
-    if(trayectoria >= 3){
-        incrementoporTrayectoria = 100000;
-        console.log(incrementoporTrayectoria);
-    }else{
-        console.log(incrementoporTrayectoria);
-    }
-}
-calculoPorTrayectoria();
+let parrafo = document.createElement("div");
+parrafo.innerHTML = "El monto que usted abonará en su permanencia de "+turno+" horas "+", será de $ "+monto;
+document.body.appendChild(parrafo); 
 
 
-function calculoporUbicacion(){
-    if(provincia == "Mendoza"){
-        incremento =150000;
-        console.log(incremento);
-    } else{
-        console.log(incremento);
-    }
-}
-calculoporUbicacion();
+let estacionamiento = document.getElementById("autosNuevos");
+estacionamiento.appendChild(parrafo); 
 
-function montoTotal() {
-    monto=(montoAprobado+incrementoporObras+incremento+incrementoporTrayectoria);
-    console.log(monto);
-}
+/* Ingreso de auto nuevo y definición de array */
 
-montoTotal();
+const baseDeautos=[];
+let marca= prompt ("Ingese la marca de su vehículo");
+let modelo=prompt("Ingrese el modelo del mismo");
+const nuevaBase=baseDeautos.concat(marca)+" "+baseDeautos.concat(modelo);
 
-alert("El monto total que su empresa puede obtener de este financiamiento es de: $ARS" + monto);
+let autos = document.getElementById("estacionamiento");
+let li = document.createElement("li");
+li.innerHTML =nuevaBase;
+autos.appendChild(li);
+
+
+
+
 
 
 
